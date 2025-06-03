021f16ce9a3a81d0ae5e93338a937c317534647e
Andres Sepulveda Velez <asepulvedadev@gmail.com>
2025-06-03 11:27:33 -0600

    Move search icon to the right

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,17 +12,16 @@ const Header = () => {
           </div>
           
           <div className="hidden md:flex md:items-center md:justify-between md:w-full md:ml-8">
-            <button className="p-2 hover:bg-red-700 rounded-full">
-              <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
-              </svg>
-            </button>
-
             <div className="text-sm">
               <span>8-9/518</span>
             </div>
 
             <div className="flex items-center space-x-4">
+              <button className="p-2 hover:bg-red-700 rounded-full">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+              </button>
               <button className="p-2 hover:bg-red-700 rounded-full">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4 6h16M4 12h16m-7 6h7" />
