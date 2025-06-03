960fe4d97da26b2ca52459d0e04dba46364f6be6
Andres Sepulveda Velez <asepulvedadev@gmail.com>
2025-06-03 12:27:54 -0600

    Fix Netlify Deployment Configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,15 @@ import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+          'supabase': ['@supabase/supabase-js']
+        }
+      }
+    }
+  }
 })
