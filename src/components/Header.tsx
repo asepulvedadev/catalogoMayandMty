6a136f687db88c62586d07d7eb837742f8e6d848
Andres Sepulveda Velez <asepulvedadev@gmail.com>
2025-06-03 11:28:36 -0600

    Center pagination in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -12,11 +12,11 @@ const Header = () => {
           </div>
           
           <div className="hidden md:flex md:items-center md:justify-between md:w-full md:ml-8">
-            <div className="text-sm">
+            <div className="flex-1"></div>
+            <div className="text-sm flex-1 text-center">
               <span>8-9/518</span>
             </div>
-
-            <div className="flex items-center space-x-4">
+            <div className="flex items-center space-x-4 flex-1 justify-end">
               <button className="p-2 hover:bg-red-700 rounded-full">
                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
